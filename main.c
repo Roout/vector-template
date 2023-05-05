3bc5c87f3359bbac253d4b3bdef26f3fc237ce57
Roout <following2future@gmail.com>
2023-05-05 14:07:06 +0300

    Following chages introduced: - vector as type is now on the stack (not elements) - add pop_back - handle malloc failure

--- main.c
+++ main.c
@@ -6,11 +6,18 @@ DEFINE_VECTOR(int_vector, int)
 DEFINE_VECTOR_INTERFACE(int_vector, int)
 
 int main() {
-    struct int_vector *v = int_vector_create(10);
-    for (size_t i = 0; i < 10; i++) {
-        int_vector_push_back(v, 5);
+    struct int_vector v;
+    if (!int_vector_init(&v, 10)) {
+        printf("Failed to init vector: not enough memory\n");
+        return 0;
     }
-    printf("%d\n", int_vector_at(v, 0));
+    for (size_t i = 0; i < 10; i++) {
+        if (!int_vector_push_back(&v, 5)) {
+            printf("Failed to push back to vector: not enough memory\n");
+            break;
+        }
+    }
+    printf("%d\n", int_vector_at(&v, 0));
     
     int_vector_destroy(&v);
     return 0;
--- vector_template.h
+++ vector_template.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <stdbool.h>
 
 #if defined(DEFINE_VECTOR)
 # error "macro definition conflict"
@@ -21,34 +22,30 @@
 # error "macro definition conflict"
 #endif
 
-#define DEFINE_VECTOR_CREATE(NAME, TYPE)                            \
-    struct NAME * NAME ## _create(size_t cap) {                     \
-        assert(cap > 0);                                            \
-        struct NAME *array = malloc(sizeof(struct NAME));           \
-        if (!array) {                                               \
-            return NULL;                                            \
-        }                                                           \
-        array->elements = malloc(cap * sizeof(TYPE));               \
-        if (!array->elements) {                                     \
-            free(array);                                            \
-            return NULL;                                            \
-        }                                                           \
-        array->capacity = cap;                                      \
-        array->size = 0;                                            \
-        return array;                                               \
+#define DEFINE_VECTOR_CREATE(NAME, TYPE)                    \
+    bool NAME ## _init(struct NAME *arr, size_t cap) {      \
+        assert(arr);                                        \
+        assert(cap > 0);                                    \
+        arr->elements = (TYPE*)malloc(cap * sizeof(TYPE));  \
+        if (!arr->elements) {                               \
+            return false;                                   \
+        }                                                   \
+        arr->capacity = cap;                                \
+        arr->size = 0;                                      \
+        return true;                                        \
     }
 
 #if defined(DEFINE_VECTOR_DESTROY)
 # error "macro definition conflict"
 #endif
 
-#define DEFINE_VECTOR_DESTROY(NAME, TYPE)                        \
-    void NAME ## _destroy(struct NAME **arr) {                   \
-        assert(arr && *arr);                                     \
-        assert((*arr)->elements);                                \
-        free((*arr)->elements);                                  \
-        free(*arr);                                              \
-        *arr = NULL;                                             \
+#define DEFINE_VECTOR_DESTROY(NAME, TYPE)                   \
+    void NAME ## _destroy(struct NAME *arr) {               \
+        assert(arr);                                        \
+        free(arr->elements);                                \
+        arr->elements = NULL;                               \
+        arr->capacity = 0;                                  \
+        arr->size = 0;                                      \
     }
 
 #if defined(DEFINE_VECTOR_PUSH_BACK)
@@ -56,7 +53,7 @@
 #endif
 
 #define DEFINE_VECTOR_PUSH_BACK(NAME, TYPE)                                     \
-    void NAME ## _push_back(struct NAME *arr, TYPE value) {                     \
+    bool NAME ## _push_back(struct NAME *arr, TYPE value) {                     \
         assert(arr);                                                            \
         assert(arr->elements);                                                  \
         if (arr->size + 1 > arr->capacity) {                                    \
@@ -64,12 +61,26 @@
             copy.size = arr->size;                                              \
             copy.capacity = arr->capacity * 2 + 1;                              \
             copy.elements = (TYPE*) malloc(sizeof(struct NAME) * copy.capacity);   \
+            if (copy.elements == NULL) return false;                               \
             memcpy(copy.elements, arr->elements, arr->size * sizeof(struct NAME)); \
             free(arr->elements);                                                \
             *arr = copy;                                                        \
         }                                                                       \
         arr->elements[arr->size] = value;                                       \
         arr->size++;                                                            \
+        return true;                                                            \
+    }
+
+#if defined(DEFINE_VECTOR_POP_BACK)
+# error "macro definition conflict"
+#endif
+
+#define DEFINE_VECTOR_POP_BACK(NAME, TYPE)                                      \
+    void NAME ## _pop_back(struct NAME *arr) {                                  \
+        assert(arr);                                                            \
+        assert(arr->elements);                                                  \
+        assert(arr->size > 0);                                                  \
+        arr->size--;                                                            \
     }
 
 #if defined(DEFINE_VECTOR_AT) || defined(DEFINE_VECTOR_CLEAR) || defined(DEFINE_VECTOR_BACK) || defined(DEFINE_VECTOR_FRONT)
@@ -111,6 +122,7 @@
     DEFINE_VECTOR_CREATE(NAME, TYPE)          \
     DEFINE_VECTOR_DESTROY(NAME, TYPE)         \
     DEFINE_VECTOR_PUSH_BACK(NAME, TYPE)       \
+    DEFINE_VECTOR_POP_BACK(NAME, TYPE)       \
     DEFINE_VECTOR_CLEAR(NAME, TYPE)           \
     DEFINE_VECTOR_BACK(NAME, TYPE)            \
     DEFINE_VECTOR_FRONT(NAME, TYPE)           \
