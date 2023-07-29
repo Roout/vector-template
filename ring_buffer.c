ef51b0a533bb18f5f79b4de697befea5da3654c1
Roout <following2future@gmail.com>
2023-07-29 15:23:08 +0300

    Finish ring buffer

--- ring_buffer.c
+++ ring_buffer.c
@@ -1,9 +1,9 @@
-#include "ring_buffer.h"
 #include <stdlib.h>
 #include <assert.h>
+#include "ring_buffer.h"
 
-bool ring_buffer_init(struct ring_buffer *buf, size_t capacity) {
-    *buf = (struct ring_buffer) {
+bool ring_buffer_init(t_ring_buffer *buf, size_t capacity) {
+    *buf = (t_ring_buffer) {
         .buffer = NULL,
         .head = 0,
         .size = 0,
@@ -13,7 +13,7 @@ bool ring_buffer_init(struct ring_buffer *buf, size_t capacity) {
     return buf->buffer != NULL;
 }
 
-void ring_buffer_push_back(struct ring_buffer *buf, time_t val) {
+void ring_buffer_push_back(t_ring_buffer *buf, time_t val) {
     assert(buf);
     assert(buf->buffer);
     assert(!ring_buffer_full(buf));
@@ -22,7 +22,7 @@ void ring_buffer_push_back(struct ring_buffer *buf, time_t val) {
     buf->size++;
 }
 
-void ring_buffer_pop_front(struct ring_buffer *buf) {
+void ring_buffer_pop_front(t_ring_buffer *buf) {
     assert(buf);
     assert(buf->buffer);
     assert(!ring_buffer_empty(buf));
@@ -30,7 +30,7 @@ void ring_buffer_pop_front(struct ring_buffer *buf) {
     buf->size--;
 }
 
-time_t* ring_buffer_at(struct ring_buffer *buf, size_t index) {
+time_t* ring_buffer_at(t_ring_buffer *buf, size_t index) {
     assert(buf);
     assert(buf->buffer);
     assert(index < buf->size);
@@ -38,24 +38,24 @@ time_t* ring_buffer_at(struct ring_buffer *buf, size_t index) {
     return &buf->buffer[transformed];
 }
 
-size_t ring_buffer_size(const struct ring_buffer *buf) {
+size_t ring_buffer_size(const t_ring_buffer *buf) {
     assert(buf);
     return buf->size;
 }
 
-bool ring_buffer_empty(const struct ring_buffer *buf) {
+bool ring_buffer_empty(const t_ring_buffer *buf) {
     assert(buf);
     return buf->size == 0;
 }
-bool ring_buffer_full(const struct ring_buffer *buf) {
+bool ring_buffer_full(const t_ring_buffer *buf) {
     assert(buf);
     return buf->size == buf->capacity;
 }
-void ring_buffer_destroy(struct ring_buffer *buf) {
+void ring_buffer_destroy(t_ring_buffer *buf) {
     free(buf->buffer);
     buf->buffer = NULL;
     buf->head = buf->size = buf->capacity = 0;
 }
-void ring_buffer_clear(struct ring_buffer *buf){
+void ring_buffer_clear(t_ring_buffer *buf){
     buf->head = buf->size = 0;
 }
--- ring_buffer.h
+++ ring_buffer.h
@@ -5,21 +5,21 @@
 #include <time.h>
 #include <stddef.h>
 
-struct ring_buffer {
+typedef struct ring_buffer {
     time_t *buffer;
     size_t head;
     size_t size;
     size_t capacity;
-};
+} t_ring_buffer;
 
-bool ring_buffer_init(struct ring_buffer *buffer, size_t size);
-void ring_buffer_push_back(struct ring_buffer *buffer, time_t val);
-void ring_buffer_pop_front(struct ring_buffer *buffer);
-time_t* ring_buffer_at(struct ring_buffer *buffer, size_t indx);
-size_t ring_buffer_size(const struct ring_buffer *buffer);
-bool ring_buffer_empty(const struct ring_buffer *buf);
-bool ring_buffer_full(const struct ring_buffer *buf);
-void ring_buffer_destroy(struct ring_buffer *buffer);
-void ring_buffer_clear(struct ring_buffer *buffer);
+bool ring_buffer_init(t_ring_buffer *buffer, size_t size);
+void ring_buffer_push_back(t_ring_buffer *buffer, time_t val);
+void ring_buffer_pop_front(t_ring_buffer *buffer);
+time_t* ring_buffer_at(t_ring_buffer *buffer, size_t indx);
+size_t ring_buffer_size(const t_ring_buffer *buffer);
+bool ring_buffer_empty(const t_ring_buffer *buf);
+bool ring_buffer_full(const t_ring_buffer *buf);
+void ring_buffer_destroy(t_ring_buffer *buffer);
+void ring_buffer_clear(t_ring_buffer *buffer);
 
 #endif // RING_BUFFER_H__
